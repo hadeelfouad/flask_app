b5702da618c990efb5c7b9fb5fab13263b09c45f
hadeelfouad <hadeel.fouad15@gmail.com>
2020-06-12 02:40:51 +0200

    validate amount and total value

--- schemas.py
+++ schemas.py
@@ -1,10 +1,10 @@
-from marshmallow import Schema, fields, ValidationError
+from marshmallow import Schema, fields, validate, ValidationError
 
 class TradeSchema(Schema):
     trade_id = fields.Int(dump_only=True)
     stock_id = fields.Str(required=True)
     user_id = fields.Int(required=True)
-    total = fields.Int(required=True) 
+    total = fields.Int(required=True, validate=[validate.Range(min=1, error="total must be greater than 0")]) 
     lower_bound = fields.Int(required=True)
     upper_bound = fields.Int(required=True)
     stock_price = fields.Int(dump_only=True)
@@ -17,5 +17,5 @@ class UserSchema(Schema):
     amount = fields.Int(default=0)
 
 class BalanceSchema(Schema):
-    amount = fields.Int(required=True)
+    amount = fields.Int(required=True, validate=[validate.Range(min=1, error="amount must be greater than 0")])
     user_id = fields.Int(required=True)
